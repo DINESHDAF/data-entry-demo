fea2b752dcbd9f8dbca5d5c46e4c58827cdbb451
DINESHDAF <dineshdf34@gmail.com>
2018-11-26 00:18:38 +0530

    Added brwsersync& gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('browser-sync', function() {
     browserSync.init({
-        proxy: "http://localhost:4200"
+        proxy: "http://localhost:4200",
+        notify: false
     });
 });
+
+gulp.task('default', ['browser-sync']);
